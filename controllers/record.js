65f8a8ed4453c14a9860ffa2cab9dce8abc15df6
Lauren Wang <wang.lauren@gmail.com>
2017-02-06 16:37:07 -0800

    single-food meals added

--- controllers/record.js
+++ controllers/record.js
@@ -9,6 +9,28 @@ const Topic = require('../lib/topic');
 
 const APP_NAME = 'record';
 
+const MEALS = [
+  {
+    id: 'id-breakfast',
+    type: 'breakfast',
+    name: 'Breakfast',
+  },
+  {
+    id: 'id-lunch',
+    type: 'lunch',
+    name: 'Lunch',
+  },
+  {
+    id: 'id-dinner',
+    type: 'dinner',
+    name: 'Dinner',
+  },
+   {
+    id: 'id-snack',
+    type: 'snack',
+    name: 'Snack',
+  },
+];
 
 //to convert to (UTC) milliseconds at local start of day
 //TODO check date is formatted correctly
@@ -35,6 +57,7 @@ function cleanedTopics(topics) {
     food: [],
     insulin: [],
     reason: [],
+    meal: MEALS,
   };
 
   topics.Items.forEach(function(topic){
@@ -52,9 +75,30 @@ function typeForRecordClass(classText) {
     return 'Insulin';
   } else if (classText === 'reading') {
     return 'Reason';
+  } else if (classText === 'meal') {
+    return 'Meal';
   } else {
     return '';
   }
+}
+
+function appendMealInfo(info, eatenThings, amounts){
+  info.quantity = 0;
+  info.mealCalories = {
+    protein: 0,
+    starch: 0,
+    veg: 0,
+    treat: 0,
+  };
+  info.mealFoods = [];
+
+  //for each food--TODO handle array
+    let eatenThing = eatenThings[0].split('|');//TOD0 check data format expected: [type, id, calories per unit]
+    let unitsEaten = amounts[0];
+    let totalCal = Math.round(eatenThing[2] * unitsEaten);
+    info.mealCalories[eatenThing[0]] = info.mealCalories[eatenThing[0]] + totalCal;
+    info.mealFoods.push(eatenThing[1] + '|' + unitsEaten + '|' + totalCal);
+    info.quantity += totalCal;
 }
 
 module.exports = function(app) {
@@ -127,6 +171,9 @@ module.exports = function(app) {
           ],
           action: {
             class: req.params.class,
+            foods: [
+              {},
+            ],
           },
           topics: cleanedTopics(topics),
 
@@ -146,27 +193,22 @@ module.exports = function(app) {
       quantity: req.body.quantity,
     };
 
+    let recordType = typeForRecordClass(info.class);
+    let topic = req.body[recordType].split('|');
+    //console.log('received topic ', topic);
+    info.type = topic[1];
+    info.topicId = topic[0];
+
     if (info.class.toLowerCase() === 'meal') {
-      // mealCalories: joi.object().keys({
-      //     all: joi.number().integer(),
-      //     protein: joi.number().integer(),
-      //     starch: joi.number().integer(),
-      //     veg: joi.number().integer(),
-      //     snack: joi.number().integer(),
-      // }).optional(),
-      // mealFoods: joi.array().items(joi.string()).single().optional(),//id<space>calories
-    } else {
-      let recordType = typeForRecordClass(info.class);
-      let topic = req.body[recordType].split('|');
-      console.log('received topic ', topic);
-      info.type = topic[1];
-      info.topicId = topic[0];
+      //appendMealInfo(info, req.body.eaten, req.body.unitsEaten);//TODO req.body.unitEaten for each member of array req.body.eaten
+      appendMealInfo(info, [req.body.eaten], [1]);
     }
 
     if (req.body.id) {
       info.date = req.body.originalDate;
       info.time = getStartOfDayMilliseconds(info.date) + (req.body.hour * 3600 + req.body.minute * 60) * 1000;
       info.id = req.body.id;
+
       Action.get(info.date, info.id, function(err, action) {
         if (err) return next(err);
         if (!action) return next(new Error('No such record found for ' + info.date));
@@ -187,6 +229,7 @@ module.exports = function(app) {
     } else {
       info.date = req.body.date;
       info.time = getStartOfDayMilliseconds(info.date) + (req.body.hour * 3600 + req.body.minute * 60) * 1000;
+
       let action = new Action(info);
 
       action.save(function(err) {
@@ -226,6 +269,16 @@ module.exports = function(app) {
           let time = new Date(actionToPassIn.time);
           actionToPassIn.minute = time.getMinutes();
           actionToPassIn.hour = time.getHours();
+          actionToPassIn.foods = actionToPassIn.mealFoods.map(function(food){
+            let temp = food.split('|');
+            return {
+              id: temp[0],
+              unit: parseFloat(temp[1]),
+              calories: parseInt(temp[2], 10),
+            };
+          });
+          delete actionToPassIn.mealFoods;
+          delete actionToPassIn.mealCalories;
 
           cb();
         });
